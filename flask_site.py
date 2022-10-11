5c93ed8ab984bcd96acc880058a3393c092e4627
Justin Donnelly <jprdonnelly@gmail.com>
2022-10-11 09:39:30 -0400

    timestamp and delete to prevent caching

--- flask_site.py
+++ flask_site.py
@@ -1,13 +1,24 @@
+import time
+import os
 from flask import Flask
 from flask import request
 from flask import render_template
-import script
+from rdkit import Chem
+from rdkit.Chem import Draw
 
 app = Flask(__name__)
+mol_name = "molecule" + str(time.time()) + ".png"
 
+for filename in os.listdir('static/'):
+    if filename.startswith('molecule_'):
+        os.remove('static/' + filename)
+
+def smiletoimage(sm):
+    my_mol = Chem.MolFromSmiles(sm)
+    Draw.MolToFile(my_mol, 'static/' + mol_name)
 
 @app.route('/')
 def my_molecule():
     param = request.args.get('smile')
-    script.smiletoimage(param)
-    return render_template("imagepage.html")
+    smiletoimage(param)
+    return render_template("imagepage.html", molecule=mol_name)
